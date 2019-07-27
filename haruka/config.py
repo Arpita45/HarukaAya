7ee16fd40cd669a2bbce810879e897a7f10dd738
Anand P S <49161738+anandvfcvijay@users.noreply.github.com>
2019-07-27 20:54:28 +0530

    Update config.py

--- haruka/config.py
+++ haruka/config.py
@@ -10,9 +10,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "731520131"
+    OWNER_ID = "658571574"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Mario_Arcus"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -23,12 +23,12 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = [658571574]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [658571574]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [658571574]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     MAPS_API = ''
     CERT_PATH = None
-    PORT = 5000
+    PORT = 8443
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_ANTISPAM = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
